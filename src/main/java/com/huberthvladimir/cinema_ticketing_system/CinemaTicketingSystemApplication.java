b496821861e989a85202d5614eb6742b0cc64077
Huberth Vladimir <vladimirhuberth@hotmail.com>
2025-02-25 12:38:51 -0300

    first commit

---
+++ src/main/java/com/huberthvladimir/cinema_ticketing_system/CinemaTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.huberthvladimir.cinema_ticketing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huberthvladimir/cinema_ticketing_system/CinemaTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huberthvladimir.cinema_ticketing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
