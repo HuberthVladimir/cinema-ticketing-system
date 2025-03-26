6dec714803bcfd206e7890914b3d354e4814930e
Huberth Vladimir <vladimirhuberth@hotmail.com>
2025-03-26 12:53:48 -0300

    feat(Genre): added method get all and add missing RequestBody

--- src/main/java/com/huberthvladimir/cinema_ticketing_system/controllers/GenreController.java
+++ src/main/java/com/huberthvladimir/cinema_ticketing_system/controllers/GenreController.java
@@ -1,5 +1,8 @@
 package com.huberthvladimir.cinema_ticketing_system.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +21,14 @@ import com.huberthvladimir.cinema_ticketing_system.services.GenreService;
 @RequestMapping(value = "/genres")
 public class GenreController {
 
+    @Autowired
     GenreService genreService;
+
+    @GetMapping
+    public ResponseEntity<List<GenreDTO>> getAllGenre() {
+        List<GenreDTO> dto = genreService.getListGenres();
+        return ResponseEntity.ok(dto);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<GenreDTO> findById(@PathVariable Long id) {
@@ -27,14 +38,14 @@ public class GenreController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
-    public ResponseEntity<GenreDTO> update(@PathVariable Long id, GenreDTO genreDTO) {
+    public ResponseEntity<GenreDTO> update(@PathVariable Long id, @RequestBody GenreDTO genreDTO) {
         GenreDTO dto = genreService.updateGenre(id, genreDTO);
         return ResponseEntity.ok(dto);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
-    public ResponseEntity<GenreDTO> create(GenreDTO genreDTO) {
+    public ResponseEntity<GenreDTO> create(@RequestBody GenreDTO genreDTO) {
         GenreDTO dto = genreService.createGenre(genreDTO);
         return ResponseEntity.ok(dto);
     }
