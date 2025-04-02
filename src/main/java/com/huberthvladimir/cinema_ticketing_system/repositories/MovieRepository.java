150289d0cb401f0b63942b8af5290a7a877af047
Huberth Vladimir <vladimirhuberth@hotmail.com>
2025-04-01 23:20:06 -0300

    fix(MovieRepository): query that returned duplicate values has been fixed

--- src/main/java/com/huberthvladimir/cinema_ticketing_system/repositories/MovieRepository.java
+++ src/main/java/com/huberthvladimir/cinema_ticketing_system/repositories/MovieRepository.java
@@ -1,6 +1,7 @@
 package com.huberthvladimir.cinema_ticketing_system.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,23 +14,25 @@ import com.huberthvladimir.cinema_ticketing_system.projections.MoviesGenresProje
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Query(nativeQuery = true, value = """
-                SELECT movies.*, genres.NAME as genre
+                SELECT movies.*, STRING_AGG(genres.NAME, ', ') as genres
                 FROM TB_MOVIES movies
                 INNER JOIN TB_MOVIES_GENRES movies_genres
                 ON movies.ID = movies_genres.MOVIE_ID
-                INNER JOIN TB_GENRES
-                genres ON genres.ID=movies_genres.GENRE_ID
+                INNER JOIN TB_GENRES genres
+                ON genres.ID = movies_genres.GENRE_ID
+                GROUP BY movies.ID;
             """)
     List<MoviesGenresProjection> searchMoviesAndGenres();
 
     @Query(nativeQuery = true, value = """
-                SELECT movies.*, genres.NAME as genre
+                SELECT movies.*, STRING_AGG(genres.NAME, ', ') as genres
                 FROM TB_MOVIES movies
                 INNER JOIN TB_MOVIES_GENRES movies_genres
                 ON movies.ID = movies_genres.MOVIE_ID
-                INNER JOIN TB_GENRES
-                genres ON genres.ID=movies_genres.GENRE_ID
+                INNER JOIN TB_GENRES genres
+                ON genres.ID = movies_genres.GENRE_ID
                 WHERE movies.ID = :id
+                GROUP BY movies.ID;
             """)
-    MoviesGenresProjection searchMovieById(Long id);
+    Optional<MoviesGenresProjection> searchMovieById(Long id);
 }
