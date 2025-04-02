5dbdb3073365b4b2f247fdc4e4e79791fce6fc63
Huberth Vladimir <vladimirhuberth@hotmail.com>
2025-04-01 23:23:51 -0300

    feat(MovieController): added crud methods

--- src/main/java/com/huberthvladimir/cinema_ticketing_system/controllers/MovieController.java
+++ src/main/java/com/huberthvladimir/cinema_ticketing_system/controllers/MovieController.java
@@ -1,10 +1,16 @@
 package com.huberthvladimir.cinema_ticketing_system.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +24,9 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+
     @GetMapping
-    public ResponseEntity<Object> getListMovies() {
+    public ResponseEntity<List<MovieDTO>> getListMovies() {
         var moviesList = movieService.getMoviesList();
         return ResponseEntity.ok(moviesList);
     }
@@ -30,9 +37,22 @@ public class MovieController {
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
-    public ResponseEntity<MovieDTO> createMovie(MovieDTO dto) {
+    public ResponseEntity<MovieDTO> createMovie(@RequestBody MovieDTO dto) {
         return ResponseEntity.ok(movieService.createMovie(dto));
-        // return ResponseEntity.ok(new MovieDTO());
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieDTO> createMovie(@PathVariable Long id, @RequestBody MovieDTO dto) {
+        return ResponseEntity.ok(movieService.updateMovie(id, dto));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
+        movieService.deleteMovie(id);
+        return ResponseEntity.ok().build();
     }
 }
